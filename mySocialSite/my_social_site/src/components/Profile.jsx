21bc2330b44b0fb66dbfaccb3f219c5032ed0167
AlexseyOstrovskiy <curtwonnengut@gmail.com>
2020-12-08 14:01:35 +0300

    08/12/2020

--- mySocialSite/my_social_site/src/components/Profile.jsx
+++ mySocialSite/my_social_site/src/components/Profile.jsx
@@ -1,27 +1,26 @@
 import React from 'react';
+import  s from './Profile.module.css';
+
 
 const Profile = () => {
-    return  <div className='content'>
+  return <div className={s.content}>
     <div>
       <img src='https://bipbap.ru/wp-content/uploads/2017/04/priroda_kartinki_foto_03.jpg'></img>
     </div>
     <div>
       ava+description
     </div>
-    <div>
+    <div className='posts' >
       My post
       <div>
         new post
       </div>
-      <div>
+      <div className={s.item}>
         Post1
-    </div>
-      <div>
+      </div>
+      <div className={s.item}>
         Post2
-    </div>
-      <div>
-        Post3
-    </div>
+      </div>
     </div>
   </div>
 }
