54811b6d1d1d019be9ce64668dbd6045a382eb5b
AlexseyOstrovskiy <curtwonnengut@gmail.com>
2021-01-12 19:13:13 +0300

    12/01/2021

--- mySocialSite/my_social_site/src/index.js
+++ mySocialSite/my_social_site/src/index.js
@@ -1,27 +1,27 @@
 import React from 'react';
 import './index.css';
-import state, { subscribe } from './redux/state';
+import store from './redux/state';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {addPost,updateNewPostText, addNewMessage,  updateNewMessageText} from './redux/state';
+
 
 let rerenderEntireTree = (state) =>{
 ReactDOM.render(
   <React.StrictMode>
     <App 
     state={state} 
-    addPost={addPost} 
-    updateNewPostText = {updateNewPostText}
-    addNewMessage = {addNewMessage}
-    updateNewMessageText ={ updateNewMessageText}
+    addPost={store.addPost.bind(store)} 
+    updateNewPostText = {store.updateNewPostText.bind(store)}
+    addNewMessage = {store.addNewMessage.bind(store)}
+    updateNewMessageText ={ store.updateNewMessageText.bind(store)}
     />
   </React.StrictMode>,
   document.getElementById('root')
 );
 }
 
-rerenderEntireTree(state);
-subscribe(rerenderEntireTree);
+rerenderEntireTree(store.getState());
+store.subscribe(rerenderEntireTree);
 
 
 
--- mySocialSite/my_social_site/src/redux/state.js
+++ mySocialSite/my_social_site/src/redux/state.js
@@ -1,73 +1,68 @@
-let rerenderEntireTree = ()=>{
-console.log('state changed')
-}
-
-let state = {
-  profilePage: {
-    posts: [
-      { id: 1, post: 'Hi, how are you?', like: 10 },
-      { id: 2, post: 'It\'is my first post!!!', like: 15 }
-    ],
-    newPostText: 'it-Kamasutra'
+let store = {
+   _state:  {
+    profilePage: {
+      posts: [
+        { id: 1, post: 'Hi, how are you?', like: 10 },
+        { id: 2, post: 'It\'is my first post!!!', like: 15 }
+      ],
+      newPostText: 'it-Kamasutra'
+    },
+  
+    messagesPage: {
+      dialogs: [
+        { id: 1, name: 'Dimych' },
+        { id: 2, name: 'Polya' },
+        { id: 3, name: 'Ragnar' },
+        { id: 4, name: 'Gertruda' },
+        { id: 5, name: 'Zidan' },
+      ],
+      messages: [
+        { id: 1, message: 'Hi, bro' },
+        { id: 2, message: 'Bro, how are yuo?' },
+        { id: 3, message: 'Yo' },
+        { id: 4, message: 'Brooo!' },
+        { id: 5, message: 'Good idea!' },
+      ],
+      newTextMessage: 'new text message !!!!!!!!!!'
+    }
   },
-
-  messagesPage: {
-    dialogs: [
-      { id: 1, name: 'Dimych' },
-      { id: 2, name: 'Polya' },
-      { id: 3, name: 'Ragnar' },
-      { id: 4, name: 'Gertruda' },
-      { id: 5, name: 'Zidan' },
-    ],
-    messages: [
-      { id: 1, message: 'Hi, bro' },
-      { id: 2, message: 'Bro, how are yuo?' },
-      { id: 3, message: 'Yo' },
-      { id: 4, message: 'Brooo!' },
-      { id: 5, message: 'Good idea!' },
-    ],
-    newTextMessage: 'new text message !!!!!!!!!!'
-  }
+  getState(){
+    return this._state;
+  },
+  _callSubscriber() {
+    console.log('state changed')
+    },
+  subscribe(observer) {
+    this._callSubscriber = observer;
+    },
+  addPost(){
+      let newPost = {
+        id: 5,
+        post: this._state.profilePage.newPostText,
+        like: 0
+      };
+      this._state.profilePage.posts.push(newPost);
+      this._state.profilePage.newPostText = " ";
+      this._callSubscriber(this._state);
+    },
+  updateNewPostText(newText){
+    this._state.profilePage.newPostText = newText;
+    this._callSubscriber(this._state);
+    },
+ addNewMessage () {
+      let newMessageState = {
+        id: this._state.messagesPage.messages.length + 1,
+        message: this._state.messagesPage.newTextMessage
+      };
+      this._state.messagesPage.messages.push(newMessageState);
+      this._state.messagesPage.newTextMessage = " ";
+      this._callSubscriber(this._state);
+    },
+updateNewMessageText(newMessage){
+  this._state.messagesPage.newTextMessage = newMessage;
+  this._callSubscriber(this._state);
+    }    
 }
 
-window.state = state;
-
-export const addPost = () => {
-  let newPost = {
-    id: 5,
-    post: state.profilePage.newPostText,
-    like: 0
-  };
-  state.profilePage.posts.push(newPost);
-  state.profilePage.newPostText = " ";
-  rerenderEntireTree(state);
-}
-
-
-export const updateNewPostText = (newText) => {
-  state.profilePage.newPostText = newText;
-
-  rerenderEntireTree(state);
-}
-
-export const addNewMessage = () => {
-  let newMessageState = {
-    id: state.messagesPage.messages.length + 1,
-    message: state.messagesPage.newTextMessage
-  };
-  state.messagesPage.messages.push(newMessageState);
-  state.messagesPage.newTextMessage = " ";
-  rerenderEntireTree(state);
-}
-
-export const updateNewMessageText = (newMessage) => {
-state.messagesPage.newTextMessage = newMessage;
-rerenderEntireTree(state);
-}
-
-export const subscribe = (observer) =>{
-  rerenderEntireTree = observer;
-}
-
-
-export default state;
+window.store = store;
+export default store;
