a71d57c8b0a9c34da1d1b8e47c94eeb3bd9a8094
AlexseyOstrovskiy <curtwonnengut@gmail.com>
2021-02-22 17:56:42 +0300

    22/02/2021 users page

--- mySocialSite/my_social_site/src/components/Users/Users.jsx
+++ mySocialSite/my_social_site/src/components/Users/Users.jsx
@@ -1,7 +1,19 @@
 import React from 'react';
-
+import s from './users.module.css'
 
 let Users =(props) =>{
+
+    if(props.users.length === 0){
+    props.setUsers([
+    { id: 1, fotoUrl:'https://yt3.ggpht.com/a/AATXAJwFh4ooyh5RaBCdE-ozqqgbR2jb0V_Sk4nNaw=s900-c-k-c0xffffffff-no-rj-mo',
+    followed:false, fullName: 'Alex', status: 'i am a boss', location :{city:'Mogilev', country:'Belarus'} },
+   { id: 2, fotoUrl:'http://pl.pollipartner.com/wp-content/uploads/2017/11/mezczyzni-850x750.jpg',
+    followed:true, fullName: 'Billy', status: 'i am a not boss', location :{city:'Moscow', country:'Russia'} },
+   { id: 3, fotoUrl:'http://pl.pollipartner.com/wp-content/uploads/2017/11/mezczyzni-850x750.jpg',
+   followed:false, fullName: 'Willy', status: 'i am drink beer now', location :{city:'Kiev', country:'Ukraine'} },]
+   );
+}
+
     return <div>
        {
            props.users.map(
@@ -12,7 +24,10 @@ let Users =(props) =>{
                             <img src={u.fotoUrl} className={s.userPhoto} />
                         </div>
                         <div>
-                            <button>Follow</button>
+                            {u.followed ?
+                            <button onClick={()=> {props.unfollow(u.id)}}  >Unfollow</button>:
+                            <button onClick={()=> {props.follow(u.id)}} >Follow</button> }
+                            
                         </div>
                     </span>
                     <span>
