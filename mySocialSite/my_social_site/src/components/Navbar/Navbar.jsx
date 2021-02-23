26ac452986d888245ea6efa793f0bd9d0be10f8c
AlexseyOstrovskiy <curtwonnengut@gmail.com>
2021-02-23 18:55:31 +0300

    23/02/2021 #53 class component

--- mySocialSite/my_social_site/src/components/Navbar/Navbar.jsx
+++ mySocialSite/my_social_site/src/components/Navbar/Navbar.jsx
@@ -18,6 +18,9 @@ const Navbar = () => {
         <div className={s.item}>
             <NavLink to ='/setting' activeClassName={s.active}  >Setting</NavLink>
         </div>
+        <div className={s.item}>
+            <NavLink to ='/users' activeClassName={s.active}  >Users</NavLink>
+        </div>
     </nav>
 }
 export default Navbar; 
