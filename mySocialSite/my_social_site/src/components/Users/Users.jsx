d72fb03536d5c9bef884cc089a52ac0d2db06bbf
AlexseyOstrovskiy <curtwonnengut@gmail.com>
2021-02-23 13:46:21 +0300

    users API get response

--- mySocialSite/my_social_site/src/components/Users/Users.jsx
+++ mySocialSite/my_social_site/src/components/Users/Users.jsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import s from './users.module.css'
-
+import * as axios from 'axios';
+import userPhotoImg from '../../assets/img/user.jpg';
 let Users =(props) =>{
 
     if(props.users.length === 0){
-    props.setUsers([
-    { id: 1, fotoUrl:'https://yt3.ggpht.com/a/AATXAJwFh4ooyh5RaBCdE-ozqqgbR2jb0V_Sk4nNaw=s900-c-k-c0xffffffff-no-rj-mo',
-    followed:false, fullName: 'Alex', status: 'i am a boss', location :{city:'Mogilev', country:'Belarus'} },
-   { id: 2, fotoUrl:'http://pl.pollipartner.com/wp-content/uploads/2017/11/mezczyzni-850x750.jpg',
-    followed:true, fullName: 'Billy', status: 'i am a not boss', location :{city:'Moscow', country:'Russia'} },
-   { id: 3, fotoUrl:'http://pl.pollipartner.com/wp-content/uploads/2017/11/mezczyzni-850x750.jpg',
-   followed:false, fullName: 'Willy', status: 'i am drink beer now', location :{city:'Kiev', country:'Ukraine'} },]
-   );
+        axios.get("https://social-network.samuraijs.com/api/1.0/users").then(response=>{
+        props.setUsers(response.data.items);
+        });
+
+    
 }
 
     return <div>
@@ -21,7 +19,7 @@ let Users =(props) =>{
                 <div key ={u.id}>
                     <span>
                         <div>
-                            <img src={u.fotoUrl} className={s.userPhoto} />
+                            <img src={u.photos.small != null ? u.photo.small :userPhotoImg} className={s.userPhoto} />
                         </div>
                         <div>
                             {u.followed ?
@@ -32,12 +30,12 @@ let Users =(props) =>{
                     </span>
                     <span>
                     <span>
-                        <div>{u.fullName}</div>
+                        <div>{u.name}</div>
                         <div>{u.status}</div>
                     </span>
                     <span>
-                        <div>{u.location.country}</div>
-                        <div>{u.location.city}</div>
+                        <div>{'u.location.country'}</div>
+                        <div>{'u.location.city'}</div>
                     </span>
                     </span>
 
