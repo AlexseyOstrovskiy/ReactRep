97e9dace936f8b2f0d0b21c25bc2ff6632a2e3b8
AlexseyOstrovskiy <curtwonnengut@gmail.com>
2021-03-07 16:18:18 +0300

    07/03/2021 homeWork pagination not working

--- mySocialSite/my_social_site/src/components/Users/Users.jsx
+++ mySocialSite/my_social_site/src/components/Users/Users.jsx
@@ -9,16 +9,54 @@ let Users = (props) => {
     for (let i = 1; i <= pagesCount; i++) {
         pages.push(i);
     }
+    let a = pages[0];
+    let b = pages[1];
+    let c = pages[2];
+    let d = pages[3]
+    let e = pages[4];
+    let f = pages[5];
+    let nextSixPages = () => {
+
+        a = a + 6;
+        b = b + 6;
+        c = c + 6;
+        d = d + 6;
+        e = e + 6;
+        f = f + 6;
+        console.log(a);
+        console.log(b);
+        console.log(c);
+        pagesSix(a,b,c,d,e,f);
+    }
+
+    let pagesSix = (a,b,c,d,e,f) => {
+        return <div>
+            <a href="#" className={props.currentPage === a && s.selectedPage} onClick={(e) => { props.onPageChanged(a) }}>{a}</a>
+            <a href="#" className={props.currentPage === b && s.selectedPage} onClick={(e) => { props.onPageChanged(b) }}>{b}</a>
+            <a href="#" className={props.currentPage === c && s.selectedPage} onClick={(e) => { props.onPageChanged(c) }}>{c}</a>
+            <a href="#" className={props.currentPage === d && s.selectedPage} onClick={(e) => { props.onPageChanged(d) }}>{d}</a>
+            <a href="#" className={props.currentPage === e && s.selectedPage} onClick={(e) => { props.onPageChanged(e) }}>{e}</a>
+            <a href="#" className={props.currentPage === f && s.selectedPage} onClick={(e) => { props.onPageChanged(f) }}>{f}</a>
+        </div>
+    }
     return <div>
-        <div className={s.stylePagination}>
+        {/* <div className={s.stylePagination}>
             {pages.map(p => {
                 return <span className={props.currentPage === p && s.selectedPage}
                     onClick={(e) => { props.onPageChanged(p) }}
                 >{p}</span>
             })}
+        </div> */}
+
+
+        <div className={s.pagination} >
+            <a href="#" >&laquo;</a>
+            <div>{pagesSix(a,b,c,d,e,f)}</div>
+            <a href="#" onClick={(e) => { nextSixPages() }}>&raquo;</a>
         </div>
 
-{/*         
+
+        {/*         
       <div className={s.pagination} >
   <a href="#">&laquo;</a>
   <a href={}>1</a>
